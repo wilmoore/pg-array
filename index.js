a3553db771b36027d4f37bbada5e2a33a332bb43
Wil Moore III <wil.moore@wilmoore.com>
2014-10-03 11:45:33 -0600

    add test/support for empty strings and arrays, null, undefined, false, and number; also, dropped utility function for converting to array and replaced with to-array.js

--- index.js
+++ index.js
@@ -1,15 +1,53 @@
 'use strict'
 
+// expose pgarray
+
 module.exports = pgarray;
 
+var toArray = require('to-array.js');
+
+/**
+ * Convert arrays and delimited strings to PostgreSQL array representation.
+ *
+ * @param {String|Array} value
+ * Delimited string or array to be converted.
+ *
+ * @param {String} [delimiter=',']
+ * String used to delimit source string (used only with string value).
+ *
+ * @return {String}
+ * PostgreSQL array formatted string.
+ */
+
 function pgarray(value, delimiter) {
-  return '{' + toArray(value, delimiter).map(trim).join(',') + '}';
+  return '{' + toArray(value, delimiter).map(clean).join(',') + '}';
 }
 
-function toArray(value, delimiter) {
-  return Array.isArray(value) ? value : value.split(delimiter || ',');
+/**
+ * Ensures value is a trimmed string.
+ *
+ * @param {*} value
+ * Value to be represented as a string literal.
+ *
+ * @return {String}
+ * Cleaned string.
+ */
+
+function clean(value) {
+  return string(value).trim();
 }
 
-function trim(value) {
-  return String(value).replace(/(^\s*|\s*$)/g, '');
+/**
+ * Return a literal string representation of given value.
+ *
+ * @param {*} value
+ * Value to be represented as a string literal.
+ *
+ * @return {String}
+ * Literal string representation of given value.
+ */
+
+function string(value) {
+  return value && value.toString ? value.toString() : '';
 }
+
--- test.js
+++ test.js
@@ -2,6 +2,12 @@ var test = require('tape');
 var pgarray = require('./');
 
 var cases = [
+  { name: 'from empty string', input: '', expected: '{}', delimiter: null },
+  { name: 'from empty array', input: [], expected: '{}', delimiter: null },
+  { name: 'from undefined', input: undefined, expected: '{}', delimiter: null },
+  { name: 'from null', input: null, expected: '{}', delimiter: null },
+  { name: 'from false', input: false, expected: '{}', delimiter: null },
+  { name: 'from number', input: 42, expected: '{42}', delimiter: null },
   { name: 'from array', input: ['erlang', 'elixir', 'python'], delimiter: null },
   { name: 'from comma delimited string (default delimiter)', input: 'erlang,elixir,python', delimiter: null },
   { name: 'from semi-colon delimited string', input: 'erlang;elixir;python', delimiter: ';' },
@@ -11,6 +17,7 @@ var cases = [
 
 cases.forEach(function (testcase) {
   test(testcase.name, function (t) {
-    t.equal('{erlang,elixir,python}', pgarray(testcase.input, testcase.delimiter)); t.end();
+    var expected = 'expected' in testcase ? testcase.expected : '{erlang,elixir,python}';
+    t.equal(expected, pgarray(testcase.input, testcase.delimiter)); t.end();
   });
 });
